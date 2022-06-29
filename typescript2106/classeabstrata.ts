5588e14b19b7a7eb04f2f341ba8311e17c91ea0b
Luiza Müller <luuiza.mullerr@gmail.com>
2022-06-28 21:44:49 -0300

    Classe Abstrata

---
+++ typescript2106/classeabstrata.ts
@@ -0,0 +1,27 @@
+abstract class Computador {
+    memoria: number = 4
+    abstract getCapacidadeProcessamento(): number
+    display(): void {
+        console.log(this.memoria)
+    }
+}
+
+class Notebook extends Computador{
+    getCapacidadeProcessamento(): number {
+        return 500
+    }
+    
+}
+
+class Tablet extends Computador{
+    getCapacidadeProcessamento(): number {
+        return 200
+    }
+    
+}
+
+let cp1: Computador //':' definindo tipo
+cp1 = new Notebook() //'=' atribuindo
+
+let cp2: Computador
+cp2 = new Tablet()
