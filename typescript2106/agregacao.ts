5e13539c2f46b9464674741e4c4beeb2aa05a794
Luiza Müller <luuiza.mullerr@gmail.com>
2022-06-29 21:49:49 -0300

    Agregação

---
+++ typescript2106/agregacao.ts
@@ -0,0 +1,39 @@
+class Product {
+    id: number
+    descricao: string
+    preco: number
+    constructor(id: number, descricao: string, preco: number) {
+        this.id = id
+        this.descricao = descricao
+        this.preco = preco
+    }
+}
+
+let prod1 = new Product(1, 'Feijão', 7.89)
+let prod2 = new Product(2, 'Carne', 2.65)
+
+class ItemVenda {
+    id: number
+    produto: Product
+    qtd: number
+    constructor(id: number, produto: Product, qtd: number) {
+        this.id = id
+        this.produto = produto
+        this.qtd = qtd
+    }
+}
+
+let item1 = new ItemVenda(1, prod1, 10)
+let item2 = new ItemVenda(2, prod2, 15)
+
+console.log(item1)
+console.log(item2)
+
+class Venda{
+    id:number
+    itens:ItemVenda[]
+    constructor (id:number, itens: ItemVenda){
+        this.id = id
+        this.itens = itens
+    }
+}
